11801a81c94232bc65ee64a604318f31624e5d48
Gowtham Raj <gowthamraj2399@gmail.com>
2024-05-31 12:25:13 +0530

    initial commit

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory } from 'history';
+import App from './App';
+
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [window.location.pathname],
+    });
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+// If we are in development and in isolation,
+// call mount immediately
+
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#_auth-dev-root');
+
+  if (el) {
+    mount(el);
+  }
+}
+
+// We are running through container
+// and we should export the mount function
+
+export { mount };
